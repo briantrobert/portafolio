b1580dad395f4bbaa95362f7f0a42e1673361233
Briant Robert <me@i-love-git.com>
2023-04-19 14:46:40 -0400

    Añadiendo iconos nuevos y agregando frame motion

---
+++ components/svg/UserNameSvg.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function UserNameSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#000"
+        d="M12 1a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM7 14a5 5 0 00-5 5v3a1 1 0 001 1h18a1 1 0 001-1v-3a5 5 0 00-5-5H7z"
+      ></path>
+    </svg>
+  );
+}
+
+export default UserNameSvg;
--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,26 @@ import NextjsSvg from '../components/svg/NextjsSvg'
 import ReactjsSvg from '../components/svg/ReactjsSvg'
 import TailwindcssSvg from '../components/svg/TailwindCssSvg'
 import TelegramIcon from '../components/svg/TelegramIcon'
+import EmailSvg from '../components/svg/EmailSvg'
+import PhoneSvg from '../components/svg/PhoneSvg'
+import UserNameSvg from '../components/svg/UserNameSvg'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <>
       {/* navbar section */}
-      <nav className="flex justify-between bg-white backdrop-blur-lg py-2">
+      <nav className="sticky top-0 z-30 flex justify-between bg-white/30 backdrop-blur-sm py-2">
         <div className="flex items-center ml-28">
           <h1 className="text-lg font-bold font-sans">Briant Robert Mengana</h1>
         </div>
         <div>
           <ul className="flex space-x-14 items-center mr-20">
+            <li className="py-1 px-2 text-lg hover:bg-slate-200 hover:shadow-2xl  hover:text-cyan-500 active:bg-slate-700 transition-all ease-in-out duration-150 font-semibold rounded-2xl">
+              <Link href="/">
+                <span>Home</span>
+              </Link>
+            </li>
             <li className="py-1 px-2 text-lg hover:bg-slate-200 hover:shadow-2xl  hover:text-cyan-500 active:bg-slate-700 transition-all ease-in-out duration-150 font-semibold rounded-2xl">
               <Link href="/">
                 <span>Portafolio</span>
@@ -27,11 +36,6 @@ export default function Home() {
             <li className="py-1 px-2 text-lg hover:bg-slate-200 hover:shadow-2xl hover:text-cyan-500 active:bg-slate-700 transition-all ease-in-out duration-150 font-semibold rounded-2xl">
               <Link href="/">
                 <span>Habilidades</span>
-              </Link>
-            </li>
-            <li className="py-1 px-2 text-lg hover:bg-slate-200 hover:shadow-2xl hover:text-cyan-500 active:bg-slate-700 transition-all ease-in-out duration-150 font-semibold rounded-2xl">
-              <Link href="/">
-                <span>Acerca de</span>
               </Link>
             </li>
             <li>
@@ -44,9 +48,12 @@ export default function Home() {
       </nav>
       {/* <!-- landing page view section--> */}
       <div className="flex h-screen w-full">
-        <div className="flex w-full h-full lg:w-1/2 items-center justify-center">
-          <div className="p-5 rounded-lg m-6">
-            <h1 className="font-bold text-4xl">I'm a web developer</h1>
+        <motion.div className="flex w-full h-full lg:w-1/2 items-center justify-center"
+         initial={{ x: '-100vw'}}
+         animate={{x: 0 }}
+         >
+          <div className="pl-24 pr-5 rounded-lg m-6">
+            <h1 className="font-bold text-3xl">I'm a Front-end Web Developer</h1>
             <h1 className="text-sm mt-2">
               Welcome, thank you to coming this far, please stay
             </h1>
@@ -56,31 +63,15 @@ export default function Home() {
               odio iure ut suscipit corporis necessitatibus assumenda dicta,
               commodi voluptatem ipsa. Dolorum!
             </h1>
-            <div className="flex mt-6 space-x-4">
-              <Link
-                className="hover:scale-110 transition-all ease-in-out duration-200"
-                href={"/"}
-              >
-                <GithubIcon />
-              </Link>
-              <Link
-                className="hover:scale-110 transition-all ease-in-out duration-200"
-                href={"/"}
-              >
-                <LinkedInIcon />
-              </Link>
-              <Link
-                className="hover:scale-110 transition-all ease-in-out duration-200"
-                href={"/"}
-              >
-                <TelegramIcon />
-              </Link>
-            </div>
+            
+            <Link href="/">
             <button class="mt-3 bg-gradient-to-tr from-pink-700 to-blue-300 border-gray-300 text-md font-semibold shadow-2xl py-2 border px-1 rounded-xl hover:scale-110 transition-all ease-in-out duration-200 active:scale-100">
-              Get started
+              Acerca de
             </button>
+
+            </Link>
           </div>
-        </div>
+        </motion.div>
         <div className="hidden lg:flex lg:w-1/2 items-center justify-center">
           <img className="md:p-15 mr-28 p-20" src="/img/landing.png" alt="" />
         </div>
@@ -177,7 +168,44 @@ export default function Home() {
       </div>
 
       {/* footer section*/}
-      <div></div>
+      <div class="mt-40 pb-4 w-full bg-gray-500 grid grid-cols-2">
+        <div class="flex flex-col lg:justify-start items-start justify-center ml-20 mt-5">
+          {/* <!-- fqltqn los iconos de los contactos --> */}
+          <div className='flex space-x-2'>
+            <UserNameSvg /><h1>Briant Robert Mengana</h1>
+          </div>
+          <div className='flex space-x-2'>
+            <PhoneSvg /><h1>+53 58815976</h1>
+          </div>
+          <div className='flex space-x-2'>
+          <EmailSvg /><h1>briantrobert89@gmail.com</h1>
+          </div>
+        </div>
+        <div class="flex flex-col items-start justify-center ml-5 mt-5">
+          <h1>Redes sociales</h1>
+          {/* <!-- faltan los boton links con los iconos github telegram y linkedIn--> */}
+          <div className="flex mt-2 space-x-4">
+              <Link
+                className="hover:scale-110 transition-all ease-in-out duration-200"
+                href={"/"}
+              >
+                <GithubIcon />
+              </Link>
+              <Link
+                className="hover:scale-110 transition-all ease-in-out duration-200"
+                href={"/"}
+              >
+                <LinkedInIcon />
+              </Link>
+              <Link
+                className="hover:scale-110 transition-all ease-in-out duration-200"
+                href={"/"}
+              >
+                <TelegramIcon />
+              </Link>
+            </div>
+        </div>
+     </div>
     </>
   );
 }
