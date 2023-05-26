25c2ef4389310721e73c7f600382915ac1061289
briant.robert <briantrobert89@gmail.com>
2023-05-26 12:02:56 +0200

    Terminando el footer

--- components/views/CardContactView.jsx
+++ components/views/CardContactView.jsx
@@ -8,50 +8,24 @@ function CardContact({locale}) {
   return (
     <div className="w-full h-full lg:flex bg-neutral-400 rounded-2xl">
       <div className="w-full lg:w-1/2 h-64">
-     
+
       </div>
       <div className="lg:w-1/2 p-3">
         <h1>
           {locale.contancName}<span className='font-semibold text-lg'>Briant Robert Mengana. </span>
           {locale.cardContactText}
         </h1>
-        <div className="pt-2">
+        <div className='pt-1'>
           <hr />
         </div>
-        <div className="pt-3">
-          <h1>{locale.contactMe}</h1>
-          <div className="flex space-x-3 pt-2 pb-2">
-            {/* <!-- faltan los boton links con los iconos github telegram y linkedIn--> */}
-           
-              <Link
-                className="hover:scale-110 transition-all ease-in-out duration-200"
-                href={"/"}
-              >
-                <GithubIcon />
-              </Link>
-              <Link
-                className="hover:scale-110 transition-all ease-in-out duration-200"
-                href={"/"}
-              >
-                <LinkedInIcon />
-              </Link>
-              <Link
-                className="hover:scale-110 transition-all ease-in-out duration-200"
-                href={"/"}
-              >
-                <TelegramIcon />
-              </Link>
-         
-          </div>
-          <hr/>
+        <div className="pt-1">
           <div className='flex justify-end pt-3'>
-          <button className="border text-gray-800 border-gray-600 hover:border-gray-800 py-1 px-1 rounded-lg mr-5 text-lg shadow-xl font-semibold hover:text-gray-800 hover:bg-orange-500 hover:scale-110 active:scale-95 transition-all ease-in-out duration-150">
-                  {locale.buttonDownloadCV}
-                </button>
+            <button className="border text-gray-800 border-gray-600 hover:border-gray-800 py-1 px-1 rounded-lg mr-5 text-lg shadow-xl font-semibold hover:text-gray-800 hover:bg-orange-500 hover:scale-110 active:scale-95 transition-all ease-in-out duration-150">
+              {locale.buttonDownloadCV}
+            </button>
           </div>
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
