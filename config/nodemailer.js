2dbde66803bef1f074649deb74a055f53b666bed
briant.robert <briantrobert89@gmail.com>
2023-10-14 23:00:56 -0400

    other see

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,6 +4,8 @@ import nodemailer from "nodemailer";
 const email = process.env.NEXT_PUBLIC_EMAIL;
 const pass = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
 
+console.log(email,pass)
+
 export const transporter = nodemailer.createTransport({
     service: 'Gmail',
     // host: "smtp.gmail.com",
