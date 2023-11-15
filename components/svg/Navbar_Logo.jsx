2ec268aac41f1ced516f574506e01bbf8cbf0405
briant.robert <briantrobert89@gmail.com>
2023-11-14 19:26:04 -0500

    COLOR svg

--- components/svg/Navbar_Logo.jsx
+++ components/svg/Navbar_Logo.jsx
@@ -74,140 +74,140 @@ function LogoSvg() {
                 xmlSpace="preserve"
               >
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M20 38a1.001 1.001 0 00-.998 1.071l1 14c.038.524.474.929.998.929h22c.524 0 .96-.405.998-.929l1-14A1 1 0 0044 38h-2.103l-.936-3.274a.996.996 0 00-.449-.584l-3.89-2.321A5.973 5.973 0 0038 28c0-3.309-2.691-6-6-6s-6 2.691-6 6c0 1.451.518 2.782 1.377 3.821l-3.89 2.321a.996.996 0 00-.449.584L22.103 38H20zm22.069 14H21.931l-.857-12H42.926l-.857 12zM32 24c2.206 0 4 1.794 4 4 0 1.31-.642 2.465-1.618 3.195l-.006.003a3.913 3.913 0 01-.511.319l-.066.036c-.171.087-.347.16-.527.22l-.09.028c-.77.24-1.595.24-2.365 0l-.089-.028a3.888 3.888 0 01-.527-.22l-.066-.036a3.954 3.954 0 01-.511-.319l-.006-.003A3.986 3.986 0 0128 28c0-2.206 1.794-4 4-4zm-7.147 11.655l4.141-2.471a5.948 5.948 0 006.012.001l4.141 2.471.67 2.344H24.183l.67-2.345zM61 56H3a1 1 0 00-1 1v4a1 1 0 001 1h58a1 1 0 001-1v-4a1 1 0 00-1-1zm-1 4H4v-2h56v2z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M47 45H49V47H47z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M51 45H53V47H51z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M55 45H57V47H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M55 41H57V43H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M55 37H57V39H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M55 33H57V35H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M55 29H57V31H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M55 25H57V27H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="##F97316"
                   d="M55 21H57V23H55z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M15 45H17V47H15z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M11 45H13V47H11z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 45H9V47H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 41H9V43H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 37H9V39H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 33H9V35H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 28H9V30H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 24H9V26H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M7 20H9V22H7z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M30 45H34V47H30z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M9.293 6.293l-3 3a.999.999 0 000 1.414l3 3 1.414-1.414L8.414 10l2.293-2.293-1.414-1.414zM20.707 6.293l-1.414 1.414L21.586 10l-2.293 2.293 1.414 1.414 3-3a.999.999 0 000-1.414l-3-3z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M10.528 9H19.472V11H10.528z"
                   data-fill-palette-color="accent"
                   transform="rotate(-63.435 15 10)"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M3 18h24a1 1 0 001-1V3a1 1 0 00-1-1H3a1 1 0 00-1 1v14a1 1 0 001 1zM4 4h22v12H4V4zM53 7c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M61 8h-.584l-.05-.122.413-.413a.997.997 0 000-1.414L57.95 3.222c-.375-.375-1.039-.375-1.415 0l-.413.413L56 3.584V3a1 1 0 00-1-1h-4a1 1 0 00-1 1v.584l-.123.051-.413-.413a1 1 0 00-1.415 0L45.22 6.051a1 1 0 000 1.414l.413.413-.05.122H45a1 1 0 00-1 1v4a1 1 0 001 1h.584l.051.123-.413.412a.997.997 0 000 1.414l2.829 2.829c.375.375 1.039.375 1.415 0l.413-.413.123.051V19a1 1 0 001 1h4a1 1 0 001-1v-.584l.123-.051.413.413a1 1 0 001.415 0l2.829-2.829a1 1 0 000-1.414l-.413-.412.051-.123H61a1 1 0 001-1V9a1 1 0 00-1-1zm-1 4h-.294a.999.999 0 00-.958.715 5.96 5.96 0 01-.468 1.138c-.211.39-.141.872.172 1.185l.205.205-1.414 1.415-.205-.206a1.006 1.006 0 00-1.185-.172 6.032 6.032 0 01-1.139.469.998.998 0 00-.714.957V18h-2v-.294a1 1 0 00-.714-.958 5.967 5.967 0 01-1.139-.469 1.003 1.003 0 00-1.185.172l-.205.206-1.414-1.415.205-.205a1 1 0 00.172-1.185 5.933 5.933 0 01-.468-1.138 1 1 0 00-.958-.714H46v-2h.294a.999.999 0 00.958-.715c.12-.403.277-.786.469-1.139a.999.999 0 00-.173-1.184l-.205-.205 1.414-1.415.205.206a1 1 0 001.185.172 6.032 6.032 0 011.139-.469.998.998 0 00.714-.957V4h2v.294a1 1 0 00.714.958c.407.122.79.279 1.139.469.389.211.871.141 1.185-.172l.205-.206 1.414 1.415-.205.205a1 1 0 00-.173 1.184c.191.353.349.735.469 1.139a1 1 0 00.958.714H60v2z"
                   data-fill-palette-color="accent"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M23.5 19.379H25.5V23.622H23.5z"
                   data-fill-palette-color="accent"
                   transform="rotate(-45.001 24.5 21.5)"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M37.379 20.5H41.622V22.5H37.379z"
                   data-fill-palette-color="accent"
                   transform="rotate(-45.001 39.5 21.5)"
                 ></path>
                 <path
-                  fill="#d72a13"
+                  fill="#F97316"
                   d="M31 15H33V18H31z"
                   data-fill-palette-color="accent"
                 ></path>
